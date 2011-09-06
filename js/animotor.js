512bdb3a7c8880548b13d21b4573c7969ea6a7d8
Timo Tuominen <timo.tuominen@gmail.com>
2011-09-06 13:00:39 +0200

    Updated vertical into a flowing list.

--- js/animotor.js
+++ js/animotor.js
@@ -5,24 +5,22 @@ var animotorObject = {
 
     calculateRatios: function(event) {
         this.spread = windowHeight;
-        //this.focusPos = event.pageY;
         this.focusPos = $(window).scrollTop() + windowHeight/2;
         this.calculate();
     },
 
     calculate: function () {
+        var pos = 0;
         _.each(this.items, _.bind(function (item) {
-            var ratio = 1.0 - (Math.abs(item.pos - this.focusPos) / this.spread)*2;
+            pos += item.elHeight / 2.0;
+            var ratio = 1.0 - (Math.abs(pos - this.focusPos) / this.spread)*2;
+            ratio *= 2.0;
             ratio = Math.min(ratio, 1.0);
             ratio = Math.max(ratio, 0.0);
+            ratio = Math.sin(ratio * Math.PI / 2);
             item.ratio = ratio;
             item.refresh();
+            pos += item.elHeight / 2.0 + item.elMarginBottom;
         }, this));
-    },
-
-    calculateDimensions: function() {
-        _.each(this.items, function (value) {
-            value.calculatePosition();
-        });
     }
 };
--- js/listitem.js
+++ js/listitem.js
@@ -3,25 +3,44 @@ var listItemObject;
 (function () {
 
     var defaultTransformation = function () {
+        if (!this.loaded) {
+            return;
+        }
+
+        var r = 0.4 + 0.6*this.ratio;
+
+        this.elWidth = this.originalWidth;
+        this.elHeight = this.originalHeight * r;
+
+        this.el.width(this.elWidth);
+        this.el.height(this.elHeight);
+
         //var transform = "scale(" + (0.8 + 0.2*this.ratio) + ")";
-        var transform = "scale(1.0," + this.ratio + ")";
+        /*var transform = "scale(1.0," + (0.4 + 0.6*this.ratio) + ")";
         this.el.css("-moz-transform", transform);
         this.el.css("-webkit-transform", transform);
-        this.el.css("transform", transform);
-    };
-
-    var calculatePositionVertical = function () {
-        var top = $(this.el).offset().top;
-        this.pos = top + $(this.el).height()/2;
+        this.el.css("transform", transform);*/
     };
 
     var createListItemEl = function () {
         this.el = $('<img alt="" src="' + this.dataEntry["media$group"]["media$thumbnail"][1].url + '"/>');
+        this.el[0].onload = _.bind(function () {
+            this.originalWidth = this.elWidth = parseInt(this.el.width());
+            this.originalHeight = this.elHeight = parseInt(this.el.height());
+            this.elMarginBottom = parseInt(this.el.css("marginBottom").replace("px", ""));
+            if (this.onload) {
+                this.onload();
+            }
+            this.loaded = true;
+        }, this);
     };
 
     listItemObject = {
+        loaded: false,
+        originalWidth: 0, originalHeight: 0,
+        elWidth: 0, elHeight: 0,
+        elMarginBottom: 0,
         refresh: defaultTransformation,
-        calculatePosition: calculatePositionVertical,
         init: createListItemEl
     };
 
