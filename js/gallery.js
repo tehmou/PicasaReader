65de9b757ac5d081ee8409b0303b88c33602a2f8
Timo Tuominen <timo.tuominen@gmail.com>
2011-09-12 14:53:48 +0200

    Chipped gallery js into gallery.js

---
+++ js/gallery.js
@@ -0,0 +1,44 @@
+var createGallery = function (options) {
+    var feed = options.feed;
+    var entries;
+
+    createModels();
+    createList();
+    start();
+
+    function createModels() {
+        entries = new Backbone.Collection(feed);
+        entries.bind("itemClick", itemClickHandler);
+
+        var aboutItem = new Backbone.Model({
+            elContent: $('<div class="blank"><span>About</span></div>')
+        });
+        var menuItem = new Backbone.Model({
+            elContent: $('<div class="blank"><span>Menu</span></div>')
+        });
+        entries.models.splice(0, 0, aboutItem, menuItem);
+        entries.models.push(new Backbone.Model({ elContent: $('<div class="blank"></div>') }));
+        entries.models.push(new Backbone.Model({ elContent: $('<div class="blank"></div>') }));
+    }
+
+    function createList() {
+        var filmlist = _.extend({}, filmListObject,
+        {
+            listItemObject: listItemObject,
+            model: entries,
+            el: $("#list-container")
+        }).init();
+        filmlist.onResize();
+        $(window)
+                .resize(filmlist.onResize)
+                .scroll(filmlist.refresh);
+    }
+
+    function itemClickHandler(item) {
+        $("#image-display").attr("src", item.get("imageUrl"));
+    }
+
+    function start() {
+        itemClickHandler(entries.at(2));
+    }
+};
