134f335fb920a2bb766342a18ea3a851cf76e94d
Timo Tuominen <timo.tuominen@gmail.com>
2011-09-19 00:11:36 +0200

    Made a fade-in animation for image display.

--- js/views/imageDisplay.js
+++ js/views/imageDisplay.js
@@ -1,8 +1,12 @@
 timotuominen.views.imageDisplay = {
     model: null,
     lastModel: null,
+    changeSpeed: 0.05,
+
+    changeRatio: 1.0,
+    loading: false,
+
     bindToModel: function (model) {
-        this.lastModel = this.model;
         this.model = model;
         this.render();
     },
@@ -10,8 +14,34 @@ timotuominen.views.imageDisplay = {
         return this;
     },
     render: function () {
-        if (this.model) {
-            this.el.attr("src", this.model.get("imageUrl"));
+        if (!this.loading) {
+
+            if (this.model != this.lastModel && this.changeRatio === 1.0) {
+                this.loading = true;
+                var image = new Image();
+                image.onload = _.bind(function() {
+                    this.el.html("");
+                    this.el.append(image);
+                    this.loading = false;
+                    this.changeRatio = 0.0;
+                    this.render();
+                }, this);
+                image.src = this.model.get("imageUrl");
+            }
+
+            if (this.changeRatio < 1.0) {
+                this.changeRatio += this.changeSpeed;
+                if (this.changeRatio >= 1.0) {
+                    this.changeRatio = 1.0;
+                } else {
+                    requestAnimationFrame(_.bind(this.render, this));
+                }
+            }
+            
+            this.el.css("opacity", this.changeRatio);
+        }
+        if (this.loading) {
+            this.el.css("opacity", 0.4);
         }
     }
 };
