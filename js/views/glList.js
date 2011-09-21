e39c3ea076a2d91724c907640b16e5989b74347c
Timo Tuominen <timo.tuominen@gmail.com>
2011-09-21 18:34:59 +0200

    Changed list WebGL rendering to a render loop.

--- js/views/glList.js
+++ js/views/glList.js
@@ -1,5 +1,4 @@
 timotuominen.views.glListObject = c0mposer.create(
-     timotuominen.gl.elements.glObject,
      {
         fragmentShaderCode: "#ifdef GL_ES\nprecision highp float;\n#endif\n\nvoid main(void) {\n\tgl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);\n}",
         vertexShaderCode: "attribute vec3 position;\nuniform mat4 uMVMatrix;\nuniform mat4 uPMatrix;\nvoid main(void) {\n\tgl_Position = uPMatrix*uMVMatrix*vec4(position.x, position.y, 0.0, 1.0);\n}",
@@ -13,6 +12,9 @@ timotuominen.views.glListObject = c0mposer.create(
             this.viewportHeight = this.el[0].height;
             this.createShader();
             this.createPlane();
+
+            this.renderLoop = _.bind(this.renderLoop, this);
+            this.renderLoop();
         },
         createShader: function () {
             this.shader = timotuominen.gl.glShaderUtils.createShader(this.gl, this.fragmentShaderCode, this.vertexShaderCode);
@@ -37,6 +39,11 @@ timotuominen.views.glListObject = c0mposer.create(
             this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.mQuadVBO);
             this.gl.bufferData(this.gl.ARRAY_BUFFER, vertices, this.gl.STATIC_DRAW);
         },
+        renderLoop: function () {
+            requestAnimationFrame(this.renderLoop);
+            this.preRender();
+            this.actualRender();
+        },
         preRender: function () {
             this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.mQuadVBO);
             this.gl.vertexAttribPointer(this.gl.getAttribLocation(this.shader, "position"), 2, this.gl.FLOAT, false, 0, 0);
@@ -49,23 +56,25 @@ timotuominen.views.glListObject = c0mposer.create(
             this.gl.viewport(0, 0, w, h);
             this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
 
-            for (var i = 0; i < this.items.length; i++) {
-                var item = this.items[i];
-                if (item.elOpacity === 0) {
-                    continue;
+            if (this.items) {
+                for (var i = 0; i < this.items.length; i++) {
+                    var item = this.items[i];
+                    if (item.elOpacity === 0) {
+                        continue;
+                    }
+                    var sx = item.elWidth/w,
+                        sy = item.elHeight/h;
+
+                    mat4.identity(m);
+                    mat4.scale(m, [sx, sy, 1.0]);
+                    this.setShaderMVMatrix(m);
+
+                    mat4.identity(m);
+                    mat4.translate(m, [item.elLeft/w, 1.0-2.0*(item.elTop + item.elHeight/2)/h, 0]);
+                    this.setShaderPMatrix(m);
+
+                    this.gl.drawArrays(this.gl.TRIANGLES, 0, 6);
                 }
-                var sx = item.elWidth/w,
-                    sy = item.elHeight/h;
-
-                mat4.identity(m);
-                mat4.scale(m, [sx, sy, 1.0]);
-                this.setShaderMVMatrix(m);
-
-                mat4.identity(m);
-                mat4.translate(m, [item.elLeft/w, 1.0-2.0*(item.elTop + item.elHeight/2)/h, 0]);
-                this.setShaderPMatrix(m);
-
-                this.gl.drawArrays(this.gl.TRIANGLES, 0, 6);
             }
         }
     },
