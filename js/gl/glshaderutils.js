5b9e5610ffe01b13c874b26c54699bf326238597
Timo Tuominen <timo.tuominen@gmail.com>
2011-09-24 15:31:46 +0200

    Added a page for testing shaders.

--- js/gl/glshaderutils.js
+++ js/gl/glshaderutils.js
@@ -1,47 +1,28 @@
 timotuominen.gl.glShaderUtils = {
-    createShader: function (gl, fragmentShaderCode, vertexShaderCode)
-    {
-        var tmpProgram = gl.createProgram(),
-            vs, fs;
-
+    createShader: function (gl, fragmentShaderCode, vertexShaderCode) {
+        var tmpProgram = gl.createProgram(), vs, fs;
         try {
-            vs = this.createVS(gl, vertexShaderCode);
-            fs = this.createFS(gl, fragmentShaderCode);
+            vs = this.compileShader(gl, vertexShaderCode, gl.VERTEX_SHADER);
+            fs = this.compileShader(gl, fragmentShaderCode, gl.FRAGMENT_SHADER);
         } catch (e) {
             gl.deleteProgram( tmpProgram );
             throw e;
         }
-
         gl.attachShader(tmpProgram, vs);
         gl.attachShader(tmpProgram, fs);
-
         gl.deleteShader(vs);
         gl.deleteShader(fs);
-
         gl.linkProgram(tmpProgram);
-
         return tmpProgram;
     },
-
-    createVS: function (gl, code) {
-        var vs = gl.createShader(gl.VERTEX_SHADER);
-        gl.shaderSource(vs, code);
-        gl.compileShader(vs);
-        if (!gl.getShaderParameter(vs, gl.COMPILE_STATUS))
+    compileShader: function (gl, code, type) {
+        var s = gl.createShader(type);
+        gl.shaderSource(s, code);
+        gl.compileShader(s);
+        if (!gl.getShaderParameter(s, gl.COMPILE_STATUS))
         {
-            throw "VS ERROR: " + gl.getShaderInfoLog(vs);
+            throw "SHADER ERROR: " + gl.getShaderInfoLog(s);
         }
-        return vs;
-    },
-
-    createFS: function (gl, code) {
-        var fs = gl.createShader(gl.FRAGMENT_SHADER);
-        gl.shaderSource(fs, code);
-        gl.compileShader(fs);
-        if (!gl.getShaderParameter(fs, gl.COMPILE_STATUS))
-        {
-            throw "FS ERROR: " + gl.getShaderInfoLog(fs);
-        }
-        return fs;
+        return s;
     }
 };
